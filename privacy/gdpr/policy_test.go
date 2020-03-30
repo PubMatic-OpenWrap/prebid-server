972aa8a17d22acf5225ac79aa3289e4917765bc3
Scott Kay <noreply@syntaxnode.com>
2020-03-30 07:48:41 -0700

    AMP CCPA Fix (#1187)

--- privacy/gdpr/policy_test.go
+++ privacy/gdpr/policy_test.go
@@ -34,3 +34,32 @@ func TestValidateConsent(t *testing.T) {
 		assert.Equal(t, test.expected, result, test.description)
 	}
 }
+
+func TestValidateConsent(t *testing.T) {
+	testCases := []struct {
+		description string
+		consent     string
+		expectError bool
+	}{
+		{
+			description: "Invalid",
+			consent:     "<any invalid>",
+			expectError: true,
+		},
+		{
+			description: "Valid",
+			consent:     "BONV8oqONXwgmADACHENAO7pqzAAppY",
+			expectError: false,
+		},
+	}
+
+	for _, test := range testCases {
+		result := ValidateConsent(test.consent)
+
+		if test.expectError {
+			assert.Error(t, result, test.description)
+		} else {
+			assert.NoError(t, result, test.description)
+		}
+	}
+}
