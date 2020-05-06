15afd47984954600cf02ef793ad2575dcd03aa87
pm-shashank-jain <shashank.jain@pubmatic.com>
2020-05-06 18:20:12 +0530

    Code Review Comment

--- exchange/targeting.go
+++ exchange/targeting.go
@@ -76,7 +76,7 @@ func (targData *targetData) setTargeting(auc *auction, isApp bool, categoryMappi
 			if len(categoryMapping) > 0 {
 				targData.addKeys(targets, openrtb_ext.HbCategoryDurationKey, categoryMapping[topBidPerBidder.bid.ID], bidderName, isOverallWinner)
 			}
-			targData.addBidderKeys(targets, topBidPerBidder.bidTargets, isOverallWinner)
+			targData.addBidderKeys(targets, topBidPerBidder.bidTargets)
 			topBidPerBidder.bidTargets = targets
 		}
 	}
@@ -98,7 +98,7 @@ func makeHbSize(bid *openrtb2.Bid) string {
 	return ""
 }
 
-func (targData *targetData) addBidderKeys(keys map[string]string, bidderKeys map[string]string, overallWinner bool) {
+func (targData *targetData) addBidderKeys(keys map[string]string, bidderKeys map[string]string) {
 	if targData.includeBidderKeys {
 		for index, element := range bidderKeys {
 			keys[index] = element
