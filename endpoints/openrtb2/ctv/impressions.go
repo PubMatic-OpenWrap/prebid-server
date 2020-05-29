d05ad00781a9bf3397b2fd2270201975c237f976
Shriprasad <shriprasad.marathe@pubmatic.com>
2020-05-29 14:50:53 +0530

    UOE-5268: Resolved conflicts

--- endpoints/openrtb2/ctv/impressions.go
+++ endpoints/openrtb2/ctv/impressions.go
@@ -236,23 +236,14 @@ func computeTimeLeastValue(time int64, leastTimeRequiredByEachSlot int64) int64 
 	leastFactor := multipleOf
 	if leastFactor < time {
 		time = leastFactor
-
-		// case:  check if slots are looking for time < leastFactor
-		// UOE-5268
-		if leastTimeRequiredByEachSlot < leastFactor {
-			time = leastTimeRequiredByEachSlot
-		}
 	}
 
-<<<<<<< HEAD
 	// case:  check if slots are looking for time < leastFactor
 	// UOE-5268
 	if leastTimeRequiredByEachSlot > 0 && leastTimeRequiredByEachSlot < time {
 		time = leastTimeRequiredByEachSlot
 	}
 
-=======
->>>>>>> ci
 	return time
 }
 
