0d7d0a8f4d178e5abd0536a3cd1d73e2129dd888
Sachin Survase <60501281+sachin-pubmatic@users.noreply.github.com>
2021-07-22 11:44:36 +0530

    Revert "UOE-6626: Supporting special characters for dctr value (#179)" (#184)
    This reverts commit b8703e5a48ce5f6d6a9c3725f0119bfa6fefc3a2.

--- adapters/pubmatic/pubmatic.go
+++ adapters/pubmatic/pubmatic.go
@@ -671,23 +671,15 @@ func parseImpressionObject(imp *openrtb2.Imp, wrapExt *pubmaticWrapperExt, pubID
 		}
 	}
 
-	imp.Ext = nil
-	if len(impExtMap) > 0 {
-		impExtBytes, err := JSONMarshal(impExtMap)
+	if len(impExtMap) != 0 {
+		impExtBytes, err := json.Marshal(impExtMap)
 		if err == nil {
 			imp.Ext = impExtBytes
 		}
 	}
 
 	return nil
-}
 
-func JSONMarshal(t interface{}) ([]byte, error) {
-	buffer := &bytes.Buffer{}
-	encoder := json.NewEncoder(buffer)
-	encoder.SetEscapeHTML(false)
-	err := encoder.Encode(t)
-	return buffer.Bytes(), err
 }
 
 func addKeywordsToExt(keywords []*openrtb_ext.ExtImpPubmaticKeyVal, extMap map[string]interface{}) {
