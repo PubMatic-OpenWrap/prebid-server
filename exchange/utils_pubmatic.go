cd74e34ce81606bc1c14d0e56f3c82e74b630d21
dhruv.sonone <dhruv.sonone@pubmatic.com>
2022-03-29 19:17:09 +0530

    Code Refactoring

--- exchange/utils_pubmatic.go
+++ exchange/utils_pubmatic.go
@@ -57,91 +57,20 @@ func deepCopyContentObj(request *openrtb2.BidRequest, contentObject *openrtb2.Co
 	if isApp {
 		app := *request.App
 		app.Content = contentObject
+		request.App = &app
 	} else {
 		site := *request.Site
 		site.Content = contentObject
+		request.Site = &site
 	}
 }
 
 func createNewContentObject(newContentObject, contentObject *openrtb2.Content, include bool, keys []string) *openrtb2.Content {
 	if include {
-		return excludeKeys(contentObject, keys)
+		return includeKeys(contentObject, keys)
 	}
-	return includeKeys(contentObject, keys)
-}
+	return excludeKeys(contentObject, keys)
 
-func includeKeys(contentObject *openrtb2.Content, keys []string) *openrtb2.Content {
-	newContentObject := *contentObject
-
-	for _, key := range keys {
-		switch key {
-		case "id":
-			newContentObject.ID = contentObject.ID
-		case "episode":
-			newContentObject.Episode = contentObject.Episode
-		case "title":
-			newContentObject.Title = contentObject.Title
-		case "series":
-			newContentObject.Series = contentObject.Series
-		case "season":
-			newContentObject.Season = contentObject.Season
-		case "artist":
-			newContentObject.Artist = contentObject.Artist
-		case "genre":
-			newContentObject.Genre = contentObject.Genre
-		case "album":
-			newContentObject.Album = contentObject.Album
-		case "isrc":
-			newContentObject.ISRC = contentObject.ISRC
-		case "producer":
-			if contentObject.Producer != nil {
-				producer := *contentObject.Producer
-				newContentObject.Producer = &producer
-			}
-		case "url":
-			newContentObject.URL = contentObject.URL
-		case "cat":
-			newContentObject.Cat = contentObject.Cat
-		case "prodq":
-			if contentObject.ProdQ != nil {
-				prodQ := *contentObject.ProdQ
-				newContentObject.ProdQ = &prodQ
-			}
-		case "videoquality":
-			if contentObject.VideoQuality != nil {
-				videoQuality := *contentObject.VideoQuality
-				newContentObject.VideoQuality = &videoQuality
-			}
-		case "context":
-			newContentObject.Context = contentObject.Context
-		case "contentrating":
-			newContentObject.ContentRating = contentObject.ContentRating
-		case "userrating":
-			newContentObject.UserRating = contentObject.UserRating
-		case "qagmediarating":
-			newContentObject.QAGMediaRating = contentObject.QAGMediaRating
-		case "keywords":
-			newContentObject.Keywords = contentObject.Keywords
-		case "livestream":
-			newContentObject.LiveStream = contentObject.LiveStream
-		case "sourcerelationship":
-			newContentObject.SourceRelationship = contentObject.SourceRelationship
-		case "len":
-			newContentObject.Len = contentObject.Len
-		case "language":
-			newContentObject.Language = contentObject.Language
-		case "embeddable":
-			newContentObject.Embeddable = contentObject.Embeddable
-		case "data":
-			if contentObject.Data != nil {
-				newContentObject.Data = contentObject.Data
-			}
-		case "ext":
-			newContentObject.Ext = contentObject.Ext
-		}
-	}
-
-	return &newContentObject
 }
 
 func excludeKeys(contentObject *openrtb2.Content, keys []string) *openrtb2.Content {
@@ -157,6 +86,26 @@ func excludeKeys(contentObject *openrtb2.Content, keys []string) *openrtb2.Conte
 		key := strings.Split(rt.Field(i).Tag.Get("json"), ",")[0] // remove omitempty, etc
 		if _, ok := keyMap[key]; ok {
 			reflect.ValueOf(&newContentObject).Elem().FieldByName(rt.Field(i).Name).Set(reflect.Zero(rt.Field(i).Type))
+		}
+	}
+
+	return &newContentObject
+}
+
+func includeKeys(contentObject *openrtb2.Content, keys []string) *openrtb2.Content {
+	newContentObject := openrtb2.Content{}
+	v := reflect.ValueOf(contentObject).Elem()
+	keyMap := make(map[string]struct{}, 1)
+	for _, key := range keys {
+		keyMap[key] = struct{}{}
+	}
+
+	rt := reflect.TypeOf(newContentObject)
+	for i := 0; i < rt.NumField(); i++ {
+		key := strings.Split(rt.Field(i).Tag.Get("json"), ",")[0] // remove omitempty, etc
+		if _, ok := keyMap[key]; ok {
+			name := rt.Field(i).Name
+			reflect.ValueOf(&newContentObject).Elem().FieldByName(rt.Field(i).Name).Set(v.FieldByName(name))
 		}
 	}
 
