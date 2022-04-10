b96461b90fa4ea48842e1681e92053a0eb7664a2
pm-nilesh-chate <nilesh.chate@pubmatic.com>
2022-04-10 06:53:13 +0000

    improve checkpoint

--- upgrade/upgrade-pbs.sh
+++ upgrade/upgrade-pbs.sh
@@ -4,15 +4,35 @@ prefix="v"
 to_major=0
 to_minor=205
 to_patch=0
+upgrade_version="$prefix$to_major.$to_minor.$to_patch"
 
 attempt=3
+
+CHECKLOG=checkpoints.log
+
+trap 'clear_log' EXIT
 
 log () {
   printf "\n$(date): $1\n"
 }
 
+clear_log() {
+    major=0
+    minor=0
+    patch=0
+    get_current_tag_version major minor patch
+    current_fork_at_version="$major.$minor.$patch"
+
+    if [ "$current_fork_at_version" == "$upgrade_version" ] ; then
+        log "Upgraded to $current_fork_at_version"
+            rm -f "$CHECKLOG"
+    else
+        log "Exiting with failure!!!"
+    fi
+}
+
 get_current_tag_version() {
-    log "get_current_tag_version $@"
+    log "get_current_tag_version $*"
 
     local -n _major=$1
     local -n _minor=$2
@@ -42,23 +62,71 @@ get_current_tag_version() {
     _patch=${_current_version[2]}
 }
 
-upgrade_version="$prefix$to_major.$to_minor.$to_patch"
+clone_repo() {
+    if [ -d "/tmp/prebid-server" ]; then
+        log "Code already cloned. Attempting to continue the upgrade!!!"
+    else
+        log "Cloning repo at /tmp"
+        cd /tmp
+        git clone https://github.com/PubMatic-OpenWrap/prebid-server.git
+        cd prebid-server
+
+        git remote add prebid-upstream https://github.com/prebid/prebid-server.git
+        git remote -v
+        git fetch --all --tags --prune
+    fi
+}
+
+checkout_branch() {
+    set +e
+    git checkout tags/$_upgrade_version -b $tag_base_branch_name
+    # git push origin $tag_base_branch_name
+    set -e
+
+    git checkout -b $upgrade_branch_name
+    # git push origin $upgrade_branch_name
+
+    if [ "$?" -ne 0 ]
+    then
+        log "Failed to create branch $upgrade_branch_name. Already working on it???"
+        exit 1
+    fi
+}
+
+cmd_exe() {
+    cmd=$*
+    if ! $cmd; then
+        log "Failure!!! creating checkpoint $cmd"
+        echo "$cmd" > $CHECKLOG
+        exit 1
+    fi
+}
+
+checkpoint_run() {
+    cmd=$*
+    if [ -f $CHECKLOG ] ; then
+        if grep -q "$cmd" "$CHECKLOG"; then
+            log "Retry this checkpoint: $cmd"
+            rm "$CHECKLOG"
+        elif grep -q "./validate.sh" "$CHECKLOG"; then
+            log "Special checkpoint. ./validate.sh failed for last tag update. Hence, only fixes are expected in successfully upgraded branch. (change in func() def, wrong conflict resolve, etc)"
+            cmd_exe $cmd
+            rm "$CHECKLOG"
+        else
+            log "Skip this checkpoint: $cmd"
+            return
+        fi
+    fi
+    cmd_exe $cmd
+}
+
+# --- main ---
+
 log "Final Upgrade Version: $upgrade_version"
 log "Attempt: $attempt"
 
-if [ -d "/tmp/prebid-server" ]; then
-    log "Code already cloned. Attempting to continue the upgrade!!!"
-    cd /tmp/prebid-server
-else
-    cd /tmp
-    git clone https://github.com/PubMatic-OpenWrap/prebid-server.git
-    cd prebid-server
-
-    git remote add prebid-upstream https://github.com/prebid/prebid-server.git
-    git remote -v
-    git fetch --all --tags --prune
-fi
-
+checkpoint_run clone_repo
+cd /tmp/prebid-server
 log "At $(pwd)"
 
 major=0
@@ -83,53 +151,23 @@ while [ "$minor" -le "$to_minor" ]; do
     log "Reference tag branch: $tag_base_branch_name"
     log "Upgrade branch: $upgrade_branch_name"
 
-    set +e
-    git checkout tags/$_upgrade_version -b $tag_base_branch_name
-    # git push origin $tag_base_branch_name
-    set -e
+    checkpoint_run checkout_branch
 
-    git checkout -b $upgrade_branch_name
-    # git push origin $upgrade_branch_name
-
-    if [ "$?" -ne 0 ]
-    then
-        log "Failed to create branch $upgrade_branch_name. Already working on it???"
-        exit 2
-    fi
-
-    git merge master --no-edit
-    git checkout master
-    git merge $upgrade_branch_name --no-edit
+    checkpoint_run git merge master --no-edit
+    checkpoint_run git checkout master
+    checkpoint_run git merge $upgrade_branch_name --no-edit
 
     git diff tags/$_upgrade_version..master > new_ow_patch_$upgrade_version-master-1.diff
 
+    # Use `git commit --amend --no-edit` if you had to fix test cases, etc for wrong merge conflict resolve, etc.
     log "Validating the merge for current tag"
-    ./validate.sh
+    go mod download all
+    go mod tidy
+    go mod tidy
+    go mod download all
+    checkpoint_run "./validate.sh"
+
+    # revert changes by ./validate.sh
+    git checkout master go.mod
+    git checkout master go.sum
 done
-
-# TODO:
-# Open draft PR between $tag_base_branch_name <- $upgrade_branch_name
-# What changes do we have on top of prebid's tag
-
-# git rebase master
-
-#todo check why code fails even with correct rebase
-
-# for each conflict
-#     1. resolve conflict
-#     2. move OpenWrap exclusive code to new _pubmatic.go file to avoid future conflicts -> TODO: DONT DO THIS HERE, BREAKS COMMIT HISTORY
-#     3. go mod tidy
-#     4. git commit -m "<SAME COMMIT MESSAGE AS OF CONFLICT. DO NOT CHANGE THIS MSG>"
-#     5. git rebase --continue
-
-# go mod tidy
-# go mod download all
-# go mod tidy
-# go mod download all
-# ./validate.sh
-
-# This commit should be of tag $upgrade_version
-# git merge-base prebid-upstream/master origin/$upgrade_branch_name
-# After master merge, this commit should be of tag $upgrade_version
-# git merge-base prebid-upstream/master origin/master
-
