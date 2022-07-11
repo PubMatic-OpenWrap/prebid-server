ff5d25f46991f59afbd371b573198ad532d90ab1
Scott Kay <noreply@syntaxnode.com>
2022-07-11 13:15:18 -0400

    OpenRTB 2.6 For Internal Model (#2280)
    * OpenRTB 2.6 Initial Commit
    
    * Fix Compilation Issues - Move To v16alpha2
    
    * Replace ExtUserEid With MXMCherry Type
    
    * Use SChain Model From MXMCherry
    
    * Review Pass

--- exchange/exchange.go
+++ exchange/exchange.go
@@ -31,7 +31,8 @@ import (
 	"github.com/buger/jsonparser"
 	"github.com/gofrs/uuid"
 	"github.com/golang/glog"
-	"github.com/mxmCherry/openrtb/v15/openrtb2"
+	"github.com/mxmCherry/openrtb/v16/openrtb2"
+	"github.com/mxmCherry/openrtb/v16/openrtb3"
 )
 
 type extCacheInstructions struct {
@@ -65,7 +66,7 @@ type exchange struct {
 	privacyConfig     config.Privacy
 	categoriesFetcher stored_requests.CategoryFetcher
 	bidIDGenerator    BidIDGenerator
-	hostSChainNode    *openrtb_ext.ExtRequestPrebidSChainSChainNode
+	hostSChainNode    *openrtb2.SupplyChainNode
 	floor             config.PriceFloors
 	trakerURL         string
 }
@@ -709,7 +710,7 @@ func (e *exchange) buildBidResponse(ctx context.Context, liveAdapters []openrtb_
 	bidResponse.ID = bidRequest.ID
 	if len(liveAdapters) == 0 {
 		// signal "Invalid Request" if no valid bidders.
-		bidResponse.NBR = openrtb2.NoBidReasonCode.Ptr(openrtb2.NoBidReasonCodeInvalidRequest)
+		bidResponse.NBR = openrtb3.NoBidInvalidRequest.Ptr()
 	}
 
 	// Create the SeatBids. We use a zero sized slice so that we can append non-zero seat bids, and not include seatBid
