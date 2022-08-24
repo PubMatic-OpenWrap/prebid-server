cc99c807a37c29c0f9991af7c252a1c9f1107102
pm-saurabh-narkhede <108730956+pm-saurabh-narkhede@users.noreply.github.com>
2022-08-24 19:25:00 +0530

    Update upgrade-pbs.sh

--- scripts/upgrade-pbs.sh
+++ scripts/upgrade-pbs.sh
@@ -148,7 +148,7 @@ cmd_exe() {
         
         if [ "$cmd"=="git merge master --no-edit" ]; then
             log "Creating Pull Request Against Current Upgrade Version Branch: $upgrade_branch_name"
-            gh pr create --title "OW Changes to be Merged in Branch $upgrade_branch_name"
+            gh pr create --repo PubMatic-OpenWrap/prebid-server -d -B $target_branch --title "Merge branch 'master' into $target_branch" --body "Resolve conflicts and continue this upgrade with '$target_branch' as input to CI"
             #Pause till Conflicts are Resolved.
         fi
         exit 1
