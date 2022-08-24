994ad34d8c621ebc255dcd8d0cccf3555b913518
pm-saurabh-narkhede <108730956+pm-saurabh-narkhede@users.noreply.github.com>
2022-08-24 17:17:17 +0530

    Update upgrade-pbs.sh

--- scripts/upgrade-pbs.sh
+++ scripts/upgrade-pbs.sh
@@ -146,10 +146,11 @@ cmd_exe() {
         log "Failure!!! creating checkpoint $cmd"
         echo "$cmd" > $CHECKLOG
         
-         if [[ $cmd -eq "git merge master --no-edit" ]]; then
-            log "Creating Pull Request Against Current Upgrade Version Branch:"
-            gh pr create --title "OW Changes to be Merged in Branch"
+        if [[ $cmd -eq "git merge master --no-edit" ]]; then
+            log "Creating Pull Request Against Current Upgrade Version Branch: $upgrade_branch_name"
+            gh pr create --title "OW Changes to be Merged in Branch $upgrade_branch_name"
             #Pause till Conflicts are Resolved.
+        fi
         exit 1
     fi
 }
