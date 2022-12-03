700b49f0cacad7d536e0cca6ad60cc7231d1055a
ashish.shinde <ashish.shinde@pubmatic.com>
2022-12-03 20:29:13 +0530

    OTT-783 : do not use stringas a context-key

--- analytics/config/config_ow.go
+++ analytics/config/config_ow.go
@@ -6,6 +6,14 @@ import (
 	"github.com/prebid/prebid-server/analytics"
 )
 
+// contextKey will be used to pass the object through request.Context
+type contextKey string
+
+const (
+	CtxWLRecord    contextKey = "wrapperLoggerRecord"
+	CtxRejectedBid contextKey = "rejectedBids"
+)
+
 func AddAnalyticsModule(moduleList, module analytics.PBSAnalyticsModule) (analytics.PBSAnalyticsModule, error) {
 	if module == nil {
 		return nil, fmt.Errorf("module to be added is nil")
