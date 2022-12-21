1b40352ed5bd3e1d2d99132860bc9c13615b80ed
supriya-patil <supriya.patil@pubmatic.com>
2022-12-21 14:57:35 +0530

    OTT-810:As a prebid server user, implement recently updated floors specifications from prebid PRD

--- floors/rule_test.go
+++ floors/rule_test.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/magiconair/properties/assert"
 	"github.com/prebid/openrtb/v17/openrtb2"
 	"github.com/prebid/prebid-server/currency"
 	"github.com/prebid/prebid-server/openrtb_ext"
@@ -493,34 +494,99 @@ func TestSelectFloorModelGroup(t *testing.T) {
 					"*|*|www.website.com":            15.01,
 					"*|*|*":                          16.01,
 				}, Default: 0.01},
+			{
+				ModelWeight:  nil,
+				SkipRate:     20,
+				ModelVersion: "Version 3",
+				Schema:       openrtb_ext.PriceFloorSchema{Fields: []string{"mediaType", "size", "domain"}},
+				Values: map[string]float64{
+					"banner|300x250|www.website.com": 1.01,
+					"banner|300x250|*":               2.01,
+					"banner|300x600|www.website.com": 3.01,
+					"banner|300x600|*":               4.01,
+					"banner|728x90|www.website.com":  5.01,
+					"banner|728x90|*":                6.01,
+					"banner|*|www.website.com":       7.01,
+					"banner|*|*":                     8.01,
+					"*|300x250|www.website.com":      9.01,
+					"*|300x250|*":                    10.01,
+					"*|300x600|www.website.com":      11.01,
+					"*|300x600|*":                    12.01,
+					"*|728x90|www.website.com":       13.01,
+					"*|728x90|*":                     14.01,
+					"*|*|www.website.com":            15.01,
+					"*|*|*":                          16.01,
+				}, Default: 0.01},
+			{
+				ModelWeight:  getIntPtr(0),
+				SkipRate:     20,
+				ModelVersion: "Version 4",
+				Schema:       openrtb_ext.PriceFloorSchema{Fields: []string{"mediaType", "size", "domain"}},
+				Values: map[string]float64{
+					"banner|300x250|www.website.com": 1.01,
+					"banner|300x250|*":               2.01,
+					"banner|300x600|www.website.com": 3.01,
+					"banner|300x600|*":               4.01,
+					"banner|728x90|www.website.com":  5.01,
+					"banner|728x90|*":                6.01,
+					"banner|*|www.website.com":       7.01,
+					"banner|*|*":                     8.01,
+					"*|300x250|www.website.com":      9.01,
+					"*|300x250|*":                    10.01,
+					"*|300x600|www.website.com":      11.01,
+					"*|300x600|*":                    12.01,
+					"*|728x90|www.website.com":       13.01,
+					"*|728x90|*":                     14.01,
+					"*|*|www.website.com":            15.01,
+					"*|*|*":                          16.01,
+				}, Default: 0.01},
 		}}}
 
 	tt := []struct {
-		name         string
-		floorExt     *openrtb_ext.PriceFloorRules
-		ModelVersion string
-		fn           func(int) int
+		name                string
+		ModelGroup openrtb_ext.PriceFloorModelGroup
+		ModelVersion        string
+		fn                  func(int) int
+		expectedModelWeight int
 	}{
 		{
-			name:         "Version 2 Selection",
-			floorExt:     floorExt,
-			ModelVersion: "Version 2",
-			fn:           func(i int) int { return 5 },
+			name:                "Version 4 Selection",
+			ModelGroup:floorExt.Data.ModelGroups[3],
+			ModelVersion:        "Version 4",
+			fn:                  func(i int) int { return 0 },
+			expectedModelWeight: 1,
 		},
 		{
-			name:         "Version 1 Selection",
-			floorExt:     floorExt,
-			ModelVersion: "Version 1",
-			fn:           func(i int) int { return 55 },
+			name:                "Version 3 Selection",
+			ModelGroup:floorExt.Data.ModelGroups[2],
+			ModelVersion:        "Version 3",
+			fn:                  func(i int) int { return 0 },
+			expectedModelWeight: 1,
+		},
+		{
+			name:                "Version 2 Selection",
+			ModelGroup:floorExt.Data.ModelGroups[1],
+			ModelVersion:        "Version 2",
+			fn:                  func(i int) int { return 5 },
+			expectedModelWeight: 25,
+		},
+		{
+			name:                "Version 1 Selection",
+			ModelGroup:floorExt.Data.ModelGroups[0],
+			ModelVersion:        "Version 1",
+			fn:                  func(i int) int { return 55 },
+			expectedModelWeight: 50,
 		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			selectFloorModelGroup(tc.floorExt.Data.ModelGroups, tc.fn)
+			resp := selectFloorModelGroup(floorExt.Data.ModelGroups, tc.fn)
 
-			if !reflect.DeepEqual(tc.floorExt.Data.ModelGroups[0].ModelVersion, tc.ModelVersion) {
-				t.Errorf("Floor Model Version mismatch error: \nreturn:\t%v\nwant:\t%v", tc.floorExt.Data.ModelGroups[0].ModelVersion, tc.ModelVersion)
+			assert.Equal(t, *resp[0].ModelWeight, tc.expectedModelWeight)
+
+			if !reflect.DeepEqual(tc.ModelGroup.ModelVersion, tc.ModelVersion) {
+				t.Errorf("Floor Model Version mismatch error: \nreturn:\t%v\nwant:\t%v", tc.ModelGroup.ModelVersion, tc.ModelVersion)
 			}
 
 		})
