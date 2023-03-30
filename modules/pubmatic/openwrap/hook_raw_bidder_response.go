b13b72607ec16c66d3883d0d12afdbf02a06c22e
Jaydeep Mohite <jaydeep.mohite@pubmatic.com>
2023-03-30 14:08:07 +0530

    Initial commid for modular approach

---
+++ modules/pubmatic/openwrap/hook_raw_bidder_response.go
@@ -0,0 +1,40 @@
+package openwrap
+
+import (
+	"fmt"
+
+	"github.com/prebid/prebid-server/adapters"
+	"github.com/prebid/prebid-server/hooks/hookstage"
+)
+
+type mediaTypes map[string]struct{}
+
+func handleRawBidderResponseHook(
+
+	payload hookstage.RawBidderResponsePayload,
+	moduleCtx hookstage.ModuleContext,
+) (result hookstage.HookResult[hookstage.RawBidderResponsePayload], err error) {
+	bidder := payload.Bidder
+
+	// allowedBids will store all bids that have passed the attribute check
+	allowedBids := make([]*adapters.TypedBid, 0)
+	for _, bid := range payload.Bids {
+
+		bidMediaTypes := mediaTypesFromBid(bid)
+
+		fmt.Printf("\n Receieved bid from = %v type = %v", bidder, bidMediaTypes)
+
+	}
+
+	changeSet := hookstage.ChangeSet[hookstage.RawBidderResponsePayload]{}
+	if len(payload.Bids) != len(allowedBids) {
+		changeSet.RawBidderResponse().Bids().Update(allowedBids)
+		result.ChangeSet = changeSet
+	}
+
+	return result, err
+}
+
+func mediaTypesFromBid(bid *adapters.TypedBid) mediaTypes {
+	return mediaTypes{string(bid.BidType): struct{}{}}
+}
