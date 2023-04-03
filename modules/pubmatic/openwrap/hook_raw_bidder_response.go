93081914bf956c661a73588e9e0282afabc60520
Jaydeep Mohite <jaydeep.mohite@pubmatic.com>
2023-04-03 08:24:37 +0530

    Added concurrent un-wrap

--- modules/pubmatic/openwrap/hook_raw_bidder_response.go
+++ modules/pubmatic/openwrap/hook_raw_bidder_response.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -15,32 +16,49 @@ import (
 
 type mediaTypes map[string]struct{}
 
+type unwrapReq struct {
+	adm       string
+	bidId     string
+	unwrapCnt int
+	err       error
+	respTime  int
+}
+
 func handleRawBidderResponseHook(
 
 	payload hookstage.RawBidderResponsePayload,
 	moduleCtx hookstage.ModuleContext,
 ) (result hookstage.HookResult[hookstage.RawBidderResponsePayload], err error) {
-	bidder := payload.Bidder
+	//bidder := payload.Bidder
 
 	// allowedBids will store all bids that have passed the attribute check
 	allowedBids := make([]*adapters.TypedBid, 0)
+
+	responseChannel := make(chan *unwrapReq, len(payload.Bids))
 	for _, bid := range payload.Bids {
 
 		bidMediaTypes := mediaTypesFromBid(bid)
 		if _, ok := bidMediaTypes["video"]; ok {
-			startTime := time.Now()
-			inLineCr, err := vastUnwrapCreative(bid.Bid.AdM)
-			respTime := int(time.Since(startTime).Milliseconds())
-			if err == nil {
-				fmt.Printf("\n Receieved bid ID = %v from = %v type = %v price = %v timeinMs = %v ", bid.Bid.ID, bidder, bidMediaTypes, bid.Bid.Price, respTime)
-				bid.Bid.AdM = inLineCr
-			} else {
-				fmt.Printf("\n vastUnwrapCreative error = %v ", err.Error())
+			go vastUnwrapCreative(bid.Bid.AdM, bid.Bid.ID, responseChannel)
+		}
+	}
+
+	unwrapCrMap := make(map[string]string, len(payload.Bids))
+	for i := 0; i < len(payload.Bids); i++ {
+		unwrapInfo := <-responseChannel
+		if unwrapInfo.err == nil {
+			unwrapCrMap[unwrapInfo.bidId] = unwrapInfo.adm
+		}
+	}
+
+	for _, bid := range payload.Bids {
+		bidMediaTypes := mediaTypesFromBid(bid)
+		if _, ok := bidMediaTypes["video"]; ok {
+			if adm, isPresent := unwrapCrMap[bid.Bid.ID]; isPresent {
+				bid.Bid.AdM = adm
 			}
 		}
-
 		allowedBids = append(allowedBids, bid)
-
 	}
 
 	changeSet := hookstage.ChangeSet[hookstage.RawBidderResponsePayload]{}
@@ -56,31 +74,41 @@ func mediaTypesFromBid(bid *adapters.TypedBid) mediaTypes {
 	return mediaTypes{string(bid.BidType): struct{}{}}
 }
 
-func vastUnwrapCreative(in string) (string, error) {
+func vastUnwrapCreative(in string, bidid string, respChan chan<- *unwrapReq) {
+	startTime := time.Now()
+	wrapperCnt := 0
 	headers := http.Header{}
 	headers.Add("Content-Type", "application/xml; charset=utf-8")
 	headers.Add("user-agent", "Mozilla/5.0 (QSP; Roku; AP; 5.4.12.227)")
 	headers.Add("unwrap-timeout", "1000")
 	httpReq, err := http.NewRequest("POST", "http://localhost:8003/unwrap", strings.NewReader(in))
 	if err != nil {
-		return in, err
+		respChan <- &unwrapReq{err: err}
 	}
 	httpReq.Header = headers
 	ctx := context.Background()
 	httpResp, err := ctxhttp.Do(ctx, nil, httpReq)
 	if err != nil {
-		return in, err
+		respChan <- &unwrapReq{err: err}
+	}
+
+	wrap_cnt := httpResp.Header.Get("unwrap-count")
+	if wrap_cnt != "" {
+		wrapperCnt, _ = strconv.Atoi(wrap_cnt)
 	}
 
 	respBody, err := io.ReadAll(httpResp.Body)
 	if err != nil {
-		return in, err
+		respChan <- &unwrapReq{err: err}
 	}
 	defer httpResp.Body.Close()
 
 	if httpResp.StatusCode != http.StatusOK {
-		return in, error(fmt.Errorf("Unexpected status code: %d. Run with request.debug = 1 for more info", httpResp.StatusCode))
+		respChan <- &unwrapReq{err: error(fmt.Errorf("Unexpected status code: %d. Run with request.debug = 1 for more info", httpResp.StatusCode))}
 	}
-	return string(respBody), nil
 
+	respTime := int(time.Since(startTime).Milliseconds())
+
+	respChan <- &unwrapReq{adm: string(respBody), bidId: bidid, unwrapCnt: wrapperCnt, respTime: respTime, err: err}
+	fmt.Printf("\n UnWrap Done for BidId = %v Cnt = %v in %v (ms)", bidid, wrapperCnt, respTime)
 }
