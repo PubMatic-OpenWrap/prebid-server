6f9e6da37ae57ad861594458c9b010d1906677ab
Nilesh Chate <nilesh.chate@pubmatic.com>
2023-04-14 10:55:23 +0530

    add seatnobid in response

--- openrtb_ext/response.go
+++ openrtb_ext/response.go
@@ -3,6 +3,7 @@ package openrtb_ext
 import (
 	"encoding/json"
 
+	"github.com/prebid/openrtb/v17/openrtb2"
 	"github.com/prebid/openrtb/v17/openrtb3"
 )
 
@@ -48,6 +49,26 @@ type ExtResponsePrebid struct {
 	Modules          json.RawMessage   `json:"modules,omitempty"`
 	Fledge           *Fledge           `json:"fledge,omitempty"`
 	Targeting        map[string]string `json:"targeting,omitempty"`
+	SeatNonBid       []SeatNonBid      `json:"seatnonbid,omitempty"`
+}
+
+// Bid is Wrapper around original/proxy bid object
+type Bid struct {
+	openrtb2.Bid
+	ID    string `json:"id,omitempty"`    // added omitempty
+	ImpID string `json:"impid,omitempty"` // added omitempty
+
+	OriginalBidCPM float64 `json:"originalbidcpm,omitempty"`
+	OriginalBidCur string  `json:"originalbidcur,omitempty"`
+}
+
+// ExtResponseNonBidPrebid represents bidresponse.ext.prebid.seatnonbid[].nonbid[].ext
+type ExtResponseNonBidPrebid struct {
+	Bid Bid `json:"bid"`
+}
+
+type NonBidExt struct {
+	Prebid ExtResponseNonBidPrebid `json:"prebid"`
 }
 
 // FledgeResponse defines the contract for bidresponse.ext.fledge
@@ -117,7 +138,7 @@ type SeatNonBid struct {
 type NonBid struct {
 	ImpId      string                    `json:"impid,omitempty"`
 	StatusCode openrtb3.NonBidStatusCode `json:"statuscode,omitempty"`
-	Ext        *ExtNonBid                `json:"ext,omitempty"`
+	Ext        NonBidExt                 `json:"ext,omitempty"`
 }
 
 // ExtNonBid defines the contract for bidresponse.ext.debug.seatnonbid.nonbid.ext
