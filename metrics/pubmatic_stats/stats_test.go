439c0c96bff3cdea46780f1c88f4167475e5e356
Shriprasad Marathe <shriprasad.marathe@pubmatic.com>
2023-05-25 18:52:31 +0530

    OTT-1060: Added test case

--- metrics/pubmatic_stats/stats_test.go
+++ metrics/pubmatic_stats/stats_test.go
@@ -5,3 +5,7 @@ import "testing"
 func TestIncBidResponseByDealCountInPBS(t *testing.T) {
 	IncBidResponseByDealCountInPBS("some_publisher_id", "some_profile_id", "some_alias_bidder", "some_dealid")
 }
+
+func TestIncPartnerTimeoutInPBS(t *testing.T) {
+	IncPartnerTimeoutInPBS("some_publisher_id", "some_profile_id", "some_alias_bidder")
+}
