97601950b2ddcffc6fcc672e28771417ac1e4898
Nikhil Vaidya <nikhil.vaidya@pubmatic.com>
2023-07-17 15:43:21 +0530

    Added impression generation algorithms

---
+++ modules/pubmatic/openwrap/models/adpod.go
@@ -0,0 +1,8 @@
+package models
+
+const (
+	// MinDuration represents index value where we can get minimum duration of given impression object
+	MinDuration = iota
+	// MaxDuration represents index value where we can get maximum duration of given impression object
+	MaxDuration
+)
