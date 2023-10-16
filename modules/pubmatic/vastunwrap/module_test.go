f2f087d136ae387c3a9a4d42f9d41a3318f0a933
ashish.shinde <ashish.shinde@pubmatic.com>
2023-10-16 16:58:56 +0530

    Fix UTs in ci branch

--- modules/pubmatic/vastunwrap/module_test.go
+++ modules/pubmatic/vastunwrap/module_test.go
@@ -250,7 +250,7 @@ func TestBuilder(t *testing.T) {
 		{
 			name: "Valid vast unwrap config",
 			args: args{
-				rawCfg: json.RawMessage(`{"enabled":true,"vastunwrap_cfg":{"app_config":{"debug":1,"unwrap_default_timeout":100},"max_wrapper_support":5,"http_config":{"idle_conn_timeout":300,"max_idle_conns":100,"max_idle_conns_per_host":1},"log_config":{"debug_log_file":"/home/test/PBSlogs/unwrap/debug.log","error_log_file":"/home/test/PBSlogs/unwrap/error.log"},"server_config":{"dc_name":"OW_DC"},"stat_config":{"host":"10.172.141.13","port":8080,"referesh_interval_in_sec":1}}}`),
+				rawCfg: json.RawMessage(`{"enabled":true,"vastunwrap_cfg":{"app_config":{"debug":1,"unwrap_default_timeout":100},"max_wrapper_support":5,"http_config":{"idle_conn_timeout":300,"max_idle_conns":100,"max_idle_conns_per_host":1},"log_config":{"debug_log_file":"/tmp/debug.log","error_log_file":"/tmp/error.log"},"server_config":{"dc_name":"OW_DC"},"stat_config":{"host":"10.172.141.13","port":8080,"referesh_interval_in_sec":1}}}`),
 				deps: moduledeps.ModuleDeps{
 					MetricsRegistry: metrics_cfg.MetricsRegistry{
 						metrics_cfg.PrometheusRegistry: prometheus.NewRegistry(),
@@ -280,7 +280,7 @@ func TestBuilder(t *testing.T) {
 		{
 			name: "Invalid vast unwrap config",
 			args: args{
-				rawCfg: json.RawMessage(`{"enabled": 1,"vastunwrap_cfg":{"app_config":{"debug":1,"unwrap_default_timeout":100},"max_wrapper_support":5,"http_config":{"idle_conn_timeout":300,"max_idle_conns":100,"max_idle_conns_per_host":1},"log_config":{"debug_log_file":"/home/test/PBSlogs/unwrap/debug.log","error_log_file":"/home/test/PBSlogs/unwrap/error.log"},"server_config":{"dc_name":"OW_DC"},"stat_config":{"host":"10.172.141.13","port":8080,"referesh_interval_in_sec":1}}}`),
+				rawCfg: json.RawMessage(`{"enabled": 1,"vastunwrap_cfg":{"app_config":{"debug":1,"unwrap_default_timeout":100},"max_wrapper_support":5,"http_config":{"idle_conn_timeout":300,"max_idle_conns":100,"max_idle_conns_per_host":1},"log_config":{"debug_log_file":"/tmp/debug.log","error_log_file":"/tmp/error.log"},"server_config":{"dc_name":"OW_DC"},"stat_config":{"host":"10.172.141.13","port":8080,"referesh_interval_in_sec":1}}}`),
 				deps: moduledeps.ModuleDeps{
 					MetricsRegistry: metrics_cfg.MetricsRegistry{
 						metrics_cfg.PrometheusRegistry: prometheus.NewRegistry(),
