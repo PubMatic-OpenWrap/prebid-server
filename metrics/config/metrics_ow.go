c0b01e315cf3e30755e17228c796589e3fcb2e10
Pubmatic-Supriya-Patil <131644110+Pubmatic-Supriya-Patil@users.noreply.github.com>
2023-10-26 10:06:03 +0530

    OTT-1319: add prometheus stats to count number of bids with vast unwrapped (#590)

--- metrics/config/metrics_ow.go
+++ metrics/config/metrics_ow.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"github.com/prebid/prebid-server/openrtb_ext"
 	"github.com/prometheus/client_golang/prometheus"
 	gometrics "github.com/rcrowley/go-metrics"
 )
@@ -19,4 +20,71 @@ func NewMetricsRegistry() MetricsRegistry {
 		PrometheusRegistry: prometheus.NewRegistry(),
 		InfluxRegistry:     gometrics.NewPrefixedRegistry("prebidserver."),
 	}
+}
+
+func (me *MultiMetricsEngine) RecordVASTTagType(biddder, vastTag string) {
+	for _, thisME := range *me {
+		thisME.RecordVASTTagType(biddder, vastTag)
+	}
+}
+
+func (me *MultiMetricsEngine) RecordRejectedBids(pubid, bidder, code string) {
+	for _, thisME := range *me {
+		thisME.RecordRejectedBids(pubid, bidder, code)
+	}
+}
+
+func (me *MultiMetricsEngine) RecordBids(pubid, profileid, biddder, deal string) {
+	for _, thisME := range *me {
+		thisME.RecordBids(pubid, profileid, biddder, deal)
+	}
+}
+func (me *MultiMetricsEngine) RecordHttpCounter() {
+}
+
+func (me *MultiMetricsEngine) RecordVastVersion(biddder, vastVersion string) {
+	for _, thisME := range *me {
+		thisME.RecordVastVersion(biddder, vastVersion)
+	}
+}
+
+// RecordRejectedBidsForBidder across all engines
+func (me *MultiMetricsEngine) RecordRejectedBidsForBidder(bidder openrtb_ext.BidderName) {
+	for _, thisME := range *me {
+		thisME.RecordRejectedBidsForBidder(bidder)
+	}
+}
+
+// RecordDynamicFetchFailure across all engines
+func (me *MultiMetricsEngine) RecordDynamicFetchFailure(pubId, code string) {
+	for _, thisME := range *me {
+		thisME.RecordDynamicFetchFailure(pubId, code)
+	}
+}
+
+// RecordVASTTagType as a noop
+func (me *NilMetricsEngine) RecordVASTTagType(biddder, vastTag string) {
+}
+
+// RecordDynamicFetchFailure as a noop
+func (me *NilMetricsEngine) RecordDynamicFetchFailure(pubId, code string) {
+}
+
+// RecordRejectedBids as a noop
+func (me *NilMetricsEngine) RecordRejectedBids(pubid, bidder, code string) {
+}
+
+// RecordBids as a noop
+func (me *NilMetricsEngine) RecordBids(pubid, profileid, biddder, deal string) {
+}
+
+// RecordVastVersion as a noop
+func (me *NilMetricsEngine) RecordVastVersion(biddder, vastVersion string) {
+}
+
+func (m *NilMetricsEngine) RecordHttpCounter() {
+}
+
+// RecordRejectedBidsForBidder as a noop
+func (me *NilMetricsEngine) RecordRejectedBidsForBidder(bidder openrtb_ext.BidderName) {
 }
