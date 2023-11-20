09adb0782e71daa8d2ab005d3b3f96be55841849
pm-aadit-patil <aadit.patil@pubmatic.com>
2023-11-20 13:57:53 +0530

    analytics format change for parsing cds

--- modules/pubmatic/openwrap/customdimensions/customdimensions.go
+++ modules/pubmatic/openwrap/customdimensions/customdimensions.go
@@ -18,15 +18,15 @@ type CustomDimension struct {
 
 // returns customDimension map if req.ext.prebid.bidderparams.pubmatic have cds
 func GetCustomDimensionsFromRequestExt(prebid *openrtb_ext.ExtRequestPrebid) map[string]CustomDimension {
-	reqCustomDimension := make(map[string]CustomDimension, 0)
 	if prebid != nil && prebid.BidderParams != nil && len(prebid.BidderParams) > 0 {
 		if cdsContent, _, _, parseErr := jsonparser.Get([]byte(prebid.BidderParams), models.BidderPubMatic, models.CustomDimensions); parseErr == nil {
+			reqCustomDimension := make(map[string]CustomDimension, 0)
 			if err := json.Unmarshal(cdsContent, &reqCustomDimension); err == nil {
 				return reqCustomDimension
 			}
 		}
 	}
-	return reqCustomDimension
+	return nil
 }
 
 // Will parse validated cds and parse/convert to string
@@ -37,11 +37,11 @@ func ParseCustomDimensionsToString(cds map[string]CustomDimension) string {
 
 	cdsSlc := []string{}
 	for k, v := range cds {
-		ele := fmt.Sprintf(`%s:%s`, k, v.Value)
+		ele := fmt.Sprintf(`%s=%s`, k, v.Value)
 		cdsSlc = append(cdsSlc, ele)
 	}
 
-	return strings.Join(cdsSlc, ",")
+	return strings.Join(cdsSlc, ";")
 }
 
 // returns if custom dimensions present in req.ext and map of customDimensiosn with attributes value and sendtoGAM
@@ -50,7 +50,9 @@ func IsCustomDimensionsPresent(ext interface{}) (map[string]CustomDimension, boo
 
 	extData, _ := json.Marshal(ext)
 	if wtExt, err := models.GetRequestExt(extData); err == nil {
-		return GetCustomDimensionsFromRequestExt(&wtExt.Prebid), true
+		if CustomDimensionsKeyValues := GetCustomDimensionsFromRequestExt(&wtExt.Prebid); CustomDimensionsKeyValues != nil {
+			return CustomDimensionsKeyValues, true
+		}
 	}
 
 	return CustomDimensionsKeyValues, false
