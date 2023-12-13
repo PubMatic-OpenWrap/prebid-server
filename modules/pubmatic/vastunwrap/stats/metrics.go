a623b2c458b78037586665ee3e52529285ba337c
Jaydeep Mohite <jaydeep.mohite@pubmatic.com>
2023-12-13 15:20:21 +0530

    OTT-1491: Added 150 ms bucket

--- modules/pubmatic/vastunwrap/stats/metrics.go
+++ modules/pubmatic/vastunwrap/stats/metrics.go
@@ -62,7 +62,7 @@ func NewMetricsEngine(cfg moduledeps.ModuleDeps) (*Metrics, error) {
 	metrics.unwrapRespTime = newHistogramVec(cfg.MetricsCfg.Prometheus, metrics.Registry,
 		"vastunwrap_resp_time",
 		"Time taken to serve the vast unwrap request in Milliseconds at wrapper count level", []string{pubIdLabel, wrapperCountLabel},
-		[]float64{50, 100, 200})
+		[]float64{50, 100, 150, 200})
 	return &metrics, nil
 }
 
