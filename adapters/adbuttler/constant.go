a4a4221a7bd4265510c871d965010b1e5f545d29
Gobi Sakthivel <gobi.sakthivel@pubmatic.com>
2024-01-09 19:19:00 +0530

    accountid change

--- adapters/adbuttler/constant.go
+++ adapters/adbuttler/constant.go
@@ -2,7 +2,7 @@ package adbuttler
 
 const (
 	BD_ZONE_ID                    = "catalogZone"
-	BD_ACCOUNT_ID                 = "retailer"
+	BD_ACCOUNT_ID                 = "accountID"
 	SEARCHTYPE_DEFAULT            = "exact"
 	SEARCHTYPE_EXACT              = "exact"
 	SEARCHTYPE_BROAD              = "broad"
