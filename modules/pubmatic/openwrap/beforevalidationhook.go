26cb7345088f0c857122b47b9a17adcea260b9c9
ashish.shinde <ashish.shinde@pubmatic.com>
2024-02-16 10:23:24 +0530

    OTT-1632: 's' object is not getting populated for owlogger for invalid-configuration

--- modules/pubmatic/openwrap/beforevalidationhook.go
+++ modules/pubmatic/openwrap/beforevalidationhook.go
@@ -114,6 +114,7 @@ func (m OpenWrap) handleBeforeValidationHook(
 			err = errors.New("failed to get profile data: received empty data")
 		}
 		result.Errors = append(result.Errors, err.Error())
+		rCtx.ImpBidCtx = getDefaultImpBidCtx(*payload.BidRequest) // for wrapper logger sz
 		m.metricEngine.RecordPublisherInvalidProfileRequests(rCtx.Endpoint, rCtx.PubIDStr, rCtx.ProfileIDStr)
 		m.metricEngine.RecordPublisherInvalidProfileImpressions(rCtx.PubIDStr, rCtx.ProfileIDStr, len(payload.BidRequest.Imp))
 		return result, err
@@ -195,6 +196,7 @@ func (m OpenWrap) handleBeforeValidationHook(
 				result.NbrCode = nbr.InternalError
 				err = errors.New("failed to parse imp.ext: " + imp.ID)
 				result.Errors = append(result.Errors, err.Error())
+				rCtx.ImpBidCtx = map[string]models.ImpCtx{} // do not create owlogger in this case
 				return result, err
 			}
 		}
@@ -205,6 +207,7 @@ func (m OpenWrap) handleBeforeValidationHook(
 			result.NbrCode = nbr.InvalidImpressionTagID
 			err = errors.New("tagid missing for imp: " + imp.ID)
 			result.Errors = append(result.Errors, err.Error())
+			rCtx.ImpBidCtx = map[string]models.ImpCtx{} // do not create owlogger in this case
 			return result, err
 		}
 
