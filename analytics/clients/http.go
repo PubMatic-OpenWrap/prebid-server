f9652430938d23936eb6b5cea8b0ea1119678725
Avinash Kapre <avinash.kapre@pubmatic.com>
2024-02-27 11:47:19 +0530

    custom client

--- analytics/clients/http.go
+++ analytics/clients/http.go
@@ -4,7 +4,13 @@ import (
 	"net/http"
 )
 
-var defaultHttpInstance = http.DefaultClient
+var defaultHttpInstance = &http.Client{
+	Transport: &http.Transport{
+		DisableKeepAlives:   false,
+		MaxIdleConnsPerHost: 1024,
+	},
+	Timeout: 15000,
+}
 
 func GetDefaultHttpInstance() *http.Client {
 	// TODO 2020-06-22 @see https://github.com/prebid/prebid-server/pull/1331#discussion_r436110097
