5f9e5309b4901dcd8d69c33cd941b5a770f53f2a
Jaydeep Mohite <jaydeep.mohite@pubmatic.com>
2024-03-18 12:26:20 +0530

    Fixed revew comment for printlog

--- modules/pubmatic/vastunwrap/unwrap_service.go
+++ modules/pubmatic/vastunwrap/unwrap_service.go
@@ -53,7 +53,7 @@ func (m VastUnwrapModule) doUnwrapandUpdateBid(isStatsEnabled bool, bid *adapter
 		bid.Bid.AdM = string(respBody)
 	}
 
-	glog.Infof("[VAST_UNWRAPPER] pubid:[%v] bidder:[%v] impid:[%v] bidid:[%v] status_code:[%v] statsEnabled:[%v]",
+	glog.V(3).Infof("[VAST_UNWRAPPER] pubid:[%v] bidder:[%v] impid:[%v] bidid:[%v] status_code:[%v] statsEnabled:[%v]",
 		accountID, bidder, bid.Bid.ImpID, bid.Bid.ID, respStatus, httpResp.Code, isStatsEnabled)
 
 }
