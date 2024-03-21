f7a507c75cfe19287c859fae3d594aef512ba9cb
supriya-patil <supriya.patil@pubmatic.com>
2024-03-21 15:57:17 +0530

    Fix: Adapter code is missing in case of seatnonbid

--- exchange/seat_non_bids.go
+++ exchange/seat_non_bids.go
@@ -17,6 +17,14 @@ func (snb *nonBids) addBid(bid *entities.PbsOrtbBid, nonBidReason int, seat stri
 	if snb.seatNonBidsMap == nil {
 		snb.seatNonBidsMap = make(map[string][]openrtb_ext.NonBid)
 	}
+	if bid.BidMeta != nil {
+		adapterCode := seat
+		if bid.AlternateBidderCode != "" {
+			adapterCode = string(openrtb_ext.BidderName(bid.AlternateBidderCode))
+		}
+		bid.BidMeta.AdapterCode = adapterCode
+	}
+
 	nonBid := openrtb_ext.NonBid{
 		ImpId:      bid.Bid.ImpID,
 		StatusCode: nonBidReason,
