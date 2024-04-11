35abc40024a2df45fa70245891bcce5f3885db36
Ankit Pinge <ankit.pinge@pubmatic.com>
2024-04-11 11:58:55 +0530

    OTT-1697 : Added support to handle  /stats and /metrics endpoint on prometheus server

--- server/prometheus.go
+++ server/prometheus.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	"github.com/golang/glog"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/prebid/prebid-server/config"
 	metricsconfig "github.com/prebid/prebid-server/metrics/config"
@@ -19,11 +18,6 @@ func newPrometheusServer(cfg *config.Configuration, metrics *metricsconfig.Detai
 	}
 	return &http.Server{
 		Addr: cfg.Host + ":" + strconv.Itoa(cfg.Metrics.Prometheus.Port),
-		Handler: promhttp.HandlerFor(proMetrics.Gatherer, promhttp.HandlerOpts{
-			ErrorLog:            loggerForPrometheus{},
-			MaxRequestsInFlight: 5,
-			Timeout:             cfg.Metrics.Prometheus.Timeout(),
-		}),
 	}
 }
 
