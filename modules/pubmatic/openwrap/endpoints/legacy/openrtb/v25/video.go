13816bd81871c9b4cafcc7de9df9a760e23339bc
Pubmatic-Supriya-Patil <131644110+Pubmatic-Supriya-Patil@users.noreply.github.com>
2024-04-19 13:52:25 +0530

    OTT-1732: fix import path (#763)

--- modules/pubmatic/openwrap/endpoints/legacy/openrtb/v25/video.go
+++ modules/pubmatic/openwrap/endpoints/legacy/openrtb/v25/video.go
@@ -8,13 +8,13 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/PubMatic-OpenWrap/prebid-server/v2/util/ptrutil"
 	"github.com/gofrs/uuid"
 	"github.com/prebid/openrtb/v20/adcom1"
 	"github.com/prebid/openrtb/v20/openrtb2"
 	"github.com/prebid/prebid-server/v2/hooks/hookstage"
 	"github.com/prebid/prebid-server/v2/modules/pubmatic/openwrap/models"
 	"github.com/prebid/prebid-server/v2/openrtb_ext"
+	"github.com/prebid/prebid-server/v2/util/ptrutil"
 )
 
 func ConvertVideoToAuctionRequest(payload hookstage.EntrypointPayload, result *hookstage.HookResult[hookstage.EntrypointPayload]) (models.RequestExtWrapper, error) {
