221204a4ea134224e948f292c4e334e2a25115de
dhruv.sonone <dhruv.sonone@pubmatic.com>
2024-05-22 14:11:33 +0530

    Adding UTs

--- modules/pubmatic/openwrap/cache/gocache/partner_config_test.go
+++ modules/pubmatic/openwrap/cache/gocache/partner_config_test.go
@@ -100,121 +100,121 @@ func TestCacheGetPartnerConfigMap(t *testing.T) {
 				},
 			},
 		},
-		// {
-		// 	name: "db_queries_failed_getting_partnerConfig_map",
-		// 	fields: fields{
-		// 		cache: gocache.New(100, 100),
-		// 		cfg: config.Cache{
-		// 			CacheDefaultExpiry: 1000,
-		// 			VASTTagCacheExpiry: 1000,
-		// 		},
-		// 	},
-		// 	args: args{
-		// 		pubID:          testPubID,
-		// 		profileID:      testProfileID,
-		// 		displayVersion: testVersionID,
-		// 	},
-		// 	setup: func(ctrl *gomock.Controller) (*mock_database.MockDatabase, *mock_metrics.MockMetricsEngine) {
-		// 		mockDatabase := mock_database.NewMockDatabase(ctrl)
-		// 		mockEngine := mock_metrics.NewMockMetricsEngine(ctrl)
-		// 		mockDatabase.EXPECT().GetActivePartnerConfigurations(testPubID, testProfileID, testVersionID).Return(nil, fmt.Errorf("Error from the DB"))
-		// 		mockDatabase.EXPECT().GetPublisherSlotNameHash(testPubID).Return(nil, fmt.Errorf("Error from the DB"))
-		// 		mockDatabase.EXPECT().GetPublisherVASTTags(testPubID).Return(nil, fmt.Errorf("Error from the DB"))
-		// 		mockEngine.EXPECT().RecordGetProfileDataTime(gomock.Any()).Return()
-		// 		mockEngine.EXPECT().RecordDBQueryFailure(models.SlotNameHash, "5890", "123").Return()
-		// 		mockEngine.EXPECT().RecordDBQueryFailure(models.PartnerConfigQuery, "5890", "123").Return()
-		// 		mockEngine.EXPECT().RecordDBQueryFailure(models.PublisherVASTTagsQuery, "5890", "123").Return()
-		// 		return mockDatabase, mockEngine
-		// 	},
-		// 	wantErr: true,
-		// 	want:    nil,
-		// },
-		// {
-		// 	name: "error_in_adunitconfig_unmarshal",
-		// 	fields: fields{
-		// 		cache: gocache.New(100, 100),
-		// 		cfg: config.Cache{
-		// 			CacheDefaultExpiry: 1000,
-		// 			VASTTagCacheExpiry: 1000,
-		// 		},
-		// 	},
-		// 	args: args{
-		// 		pubID:          testPubID,
-		// 		profileID:      testProfileID,
-		// 		displayVersion: 0,
-		// 	},
-		// 	setup: func(ctrl *gomock.Controller) (*mock_database.MockDatabase, *mock_metrics.MockMetricsEngine) {
-		// 		mockDatabase := mock_database.NewMockDatabase(ctrl)
-		// 		mockEngine := mock_metrics.NewMockMetricsEngine(ctrl)
-		// 		mockDatabase.EXPECT().GetActivePartnerConfigurations(testPubID, testProfileID, 0).Return(formTestPartnerConfig(), nil)
-		// 		mockDatabase.EXPECT().GetPublisherSlotNameHash(testPubID).Return(map[string]string{"adunit@728x90": "2aa34b52a9e941c1594af7565e599c8d"}, nil)
-		// 		mockDatabase.EXPECT().GetPublisherVASTTags(testPubID).Return(nil, nil)
-		// 		mockDatabase.EXPECT().GetAdunitConfig(testProfileID, 0).Return(nil, adunitconfig.ErrAdUnitUnmarshal)
-		// 		mockDatabase.EXPECT().GetWrapperSlotMappings(formTestPartnerConfig(), testProfileID, 0).Return(nil, nil)
-		// 		mockEngine.EXPECT().RecordGetProfileDataTime(gomock.Any()).Return().Times(1)
-		// 		mockEngine.EXPECT().RecordDBQueryFailure(models.AdUnitFailUnmarshal, "5890", "123").Return().Times(1)
-		// 		return mockDatabase, mockEngine
-		// 	},
-		// 	wantErr: true,
-		// 	want:    nil,
-		// },
-		// {
-		// 	name: "db_queries_failed_getting_adunitconfig",
-		// 	fields: fields{
-		// 		cache: gocache.New(100, 100),
-		// 		cfg: config.Cache{
-		// 			CacheDefaultExpiry: 1000,
-		// 			VASTTagCacheExpiry: 1000,
-		// 		},
-		// 	},
-		// 	args: args{
-		// 		pubID:          testPubID,
-		// 		profileID:      testProfileID,
-		// 		displayVersion: 0,
-		// 	},
-		// 	setup: func(ctrl *gomock.Controller) (*mock_database.MockDatabase, *mock_metrics.MockMetricsEngine) {
-		// 		mockDatabase := mock_database.NewMockDatabase(ctrl)
-		// 		mockEngine := mock_metrics.NewMockMetricsEngine(ctrl)
-		// 		mockDatabase.EXPECT().GetActivePartnerConfigurations(testPubID, testProfileID, 0).Return(formTestPartnerConfig(), nil)
-		// 		mockDatabase.EXPECT().GetPublisherSlotNameHash(testPubID).Return(map[string]string{"adunit@728x90": "2aa34b52a9e941c1594af7565e599c8d"}, nil)
-		// 		mockDatabase.EXPECT().GetPublisherVASTTags(testPubID).Return(nil, nil)
-		// 		mockDatabase.EXPECT().GetAdunitConfig(testProfileID, 0).Return(nil, errors.New("Failed to connect DB"))
-		// 		mockDatabase.EXPECT().GetWrapperSlotMappings(formTestPartnerConfig(), testProfileID, 0).Return(nil, nil)
-		// 		mockEngine.EXPECT().RecordGetProfileDataTime(gomock.Any()).Return().Times(1)
-		// 		mockEngine.EXPECT().RecordDBQueryFailure(models.AdunitConfigForLiveVersion, "5890", "123").Return().Times(1)
-		// 		return mockDatabase, mockEngine
-		// 	},
-		// 	wantErr: true,
-		// 	want:    nil,
-		// },
-		// {
-		// 	name: "db_queries_failed_getting_wrapper_slotmappings",
-		// 	fields: fields{
-		// 		cache: gocache.New(100, 100),
-		// 		cfg: config.Cache{
-		// 			CacheDefaultExpiry: 1000,
-		// 			VASTTagCacheExpiry: 1000,
-		// 		},
-		// 	},
-		// 	args: args{
-		// 		pubID:          testPubID,
-		// 		profileID:      testProfileID,
-		// 		displayVersion: 0,
-		// 	},
-		// 	setup: func(ctrl *gomock.Controller) (*mock_database.MockDatabase, *mock_metrics.MockMetricsEngine) {
-		// 		mockDatabase := mock_database.NewMockDatabase(ctrl)
-		// 		mockEngine := mock_metrics.NewMockMetricsEngine(ctrl)
-		// 		mockDatabase.EXPECT().GetActivePartnerConfigurations(testPubID, testProfileID, 0).Return(formTestPartnerConfig(), nil)
-		// 		mockDatabase.EXPECT().GetPublisherSlotNameHash(testPubID).Return(map[string]string{"adunit@728x90": "2aa34b52a9e941c1594af7565e599c8d"}, nil)
-		// 		mockDatabase.EXPECT().GetPublisherVASTTags(testPubID).Return(nil, nil)
-		// 		mockDatabase.EXPECT().GetWrapperSlotMappings(formTestPartnerConfig(), testProfileID, 0).Return(nil, fmt.Errorf("Error from the DB"))
-		// 		mockEngine.EXPECT().RecordGetProfileDataTime(gomock.Any()).Return().Times(1)
-		// 		mockEngine.EXPECT().RecordDBQueryFailure(models.WrapperLiveVersionSlotMappings, "5890", "123").Return().Times(1)
-		// 		return mockDatabase, mockEngine
-		// 	},
-		// 	wantErr: true,
-		// 	want:    nil,
-		// },
+		{
+			name: "db_queries_failed_getting_partnerConfig_map",
+			fields: fields{
+				cache: gocache.New(100, 100),
+				cfg: config.Cache{
+					CacheDefaultExpiry: 1000,
+					VASTTagCacheExpiry: 1000,
+				},
+			},
+			args: args{
+				pubID:          testPubID,
+				profileID:      testProfileID,
+				displayVersion: testVersionID,
+			},
+			setup: func(ctrl *gomock.Controller) (*mock_database.MockDatabase, *mock_metrics.MockMetricsEngine) {
+				mockDatabase := mock_database.NewMockDatabase(ctrl)
+				mockEngine := mock_metrics.NewMockMetricsEngine(ctrl)
+				mockDatabase.EXPECT().GetActivePartnerConfigurations(testPubID, testProfileID, testVersionID).Return(nil, fmt.Errorf("Error from the DB"))
+				mockDatabase.EXPECT().GetPublisherSlotNameHash(testPubID).Return(nil, fmt.Errorf("Error from the DB"))
+				mockDatabase.EXPECT().GetPublisherVASTTags(testPubID).Return(nil, fmt.Errorf("Error from the DB"))
+				mockEngine.EXPECT().RecordGetProfileDataTime(gomock.Any()).Return()
+				mockEngine.EXPECT().RecordDBQueryFailure(models.SlotNameHash, "5890", "123").Return()
+				mockEngine.EXPECT().RecordDBQueryFailure(models.PartnerConfigQuery, "5890", "123").Return()
+				mockEngine.EXPECT().RecordDBQueryFailure(models.PublisherVASTTagsQuery, "5890", "123").Return()
+				return mockDatabase, mockEngine
+			},
+			wantErr: true,
+			want:    nil,
+		},
+		{
+			name: "error_in_adunitconfig_unmarshal",
+			fields: fields{
+				cache: gocache.New(100, 100),
+				cfg: config.Cache{
+					CacheDefaultExpiry: 1000,
+					VASTTagCacheExpiry: 1000,
+				},
+			},
+			args: args{
+				pubID:          testPubID,
+				profileID:      testProfileID,
+				displayVersion: 0,
+			},
+			setup: func(ctrl *gomock.Controller) (*mock_database.MockDatabase, *mock_metrics.MockMetricsEngine) {
+				mockDatabase := mock_database.NewMockDatabase(ctrl)
+				mockEngine := mock_metrics.NewMockMetricsEngine(ctrl)
+				mockDatabase.EXPECT().GetActivePartnerConfigurations(testPubID, testProfileID, 0).Return(formTestPartnerConfig(), nil)
+				mockDatabase.EXPECT().GetPublisherSlotNameHash(testPubID).Return(map[string]string{"adunit@728x90": "2aa34b52a9e941c1594af7565e599c8d"}, nil)
+				mockDatabase.EXPECT().GetPublisherVASTTags(testPubID).Return(nil, nil)
+				mockDatabase.EXPECT().GetAdunitConfig(testProfileID, 0).Return(nil, adunitconfig.ErrAdUnitUnmarshal)
+				mockDatabase.EXPECT().GetWrapperSlotMappings(formTestPartnerConfig(), testProfileID, 0).Return(nil, nil)
+				mockEngine.EXPECT().RecordGetProfileDataTime(gomock.Any()).Return().Times(1)
+				mockEngine.EXPECT().RecordDBQueryFailure(models.AdUnitFailUnmarshal, "5890", "123").Return().Times(1)
+				return mockDatabase, mockEngine
+			},
+			wantErr: true,
+			want:    nil,
+		},
+		{
+			name: "db_queries_failed_getting_adunitconfig",
+			fields: fields{
+				cache: gocache.New(100, 100),
+				cfg: config.Cache{
+					CacheDefaultExpiry: 1000,
+					VASTTagCacheExpiry: 1000,
+				},
+			},
+			args: args{
+				pubID:          testPubID,
+				profileID:      testProfileID,
+				displayVersion: 0,
+			},
+			setup: func(ctrl *gomock.Controller) (*mock_database.MockDatabase, *mock_metrics.MockMetricsEngine) {
+				mockDatabase := mock_database.NewMockDatabase(ctrl)
+				mockEngine := mock_metrics.NewMockMetricsEngine(ctrl)
+				mockDatabase.EXPECT().GetActivePartnerConfigurations(testPubID, testProfileID, 0).Return(formTestPartnerConfig(), nil)
+				mockDatabase.EXPECT().GetPublisherSlotNameHash(testPubID).Return(map[string]string{"adunit@728x90": "2aa34b52a9e941c1594af7565e599c8d"}, nil)
+				mockDatabase.EXPECT().GetPublisherVASTTags(testPubID).Return(nil, nil)
+				mockDatabase.EXPECT().GetAdunitConfig(testProfileID, 0).Return(nil, errors.New("Failed to connect DB"))
+				mockDatabase.EXPECT().GetWrapperSlotMappings(formTestPartnerConfig(), testProfileID, 0).Return(nil, nil)
+				mockEngine.EXPECT().RecordGetProfileDataTime(gomock.Any()).Return().Times(1)
+				mockEngine.EXPECT().RecordDBQueryFailure(models.AdunitConfigForLiveVersion, "5890", "123").Return().Times(1)
+				return mockDatabase, mockEngine
+			},
+			wantErr: true,
+			want:    nil,
+		},
+		{
+			name: "db_queries_failed_getting_wrapper_slotmappings",
+			fields: fields{
+				cache: gocache.New(100, 100),
+				cfg: config.Cache{
+					CacheDefaultExpiry: 1000,
+					VASTTagCacheExpiry: 1000,
+				},
+			},
+			args: args{
+				pubID:          testPubID,
+				profileID:      testProfileID,
+				displayVersion: 0,
+			},
+			setup: func(ctrl *gomock.Controller) (*mock_database.MockDatabase, *mock_metrics.MockMetricsEngine) {
+				mockDatabase := mock_database.NewMockDatabase(ctrl)
+				mockEngine := mock_metrics.NewMockMetricsEngine(ctrl)
+				mockDatabase.EXPECT().GetActivePartnerConfigurations(testPubID, testProfileID, 0).Return(formTestPartnerConfig(), nil)
+				mockDatabase.EXPECT().GetPublisherSlotNameHash(testPubID).Return(map[string]string{"adunit@728x90": "2aa34b52a9e941c1594af7565e599c8d"}, nil)
+				mockDatabase.EXPECT().GetPublisherVASTTags(testPubID).Return(nil, nil)
+				mockDatabase.EXPECT().GetWrapperSlotMappings(formTestPartnerConfig(), testProfileID, 0).Return(nil, fmt.Errorf("Error from the DB"))
+				mockEngine.EXPECT().RecordGetProfileDataTime(gomock.Any()).Return().Times(1)
+				mockEngine.EXPECT().RecordDBQueryFailure(models.WrapperLiveVersionSlotMappings, "5890", "123").Return().Times(1)
+				return mockDatabase, mockEngine
+			},
+			wantErr: true,
+			want:    nil,
+		},
 	}
 	for ind := range tests {
 		tt := &tests[ind]
