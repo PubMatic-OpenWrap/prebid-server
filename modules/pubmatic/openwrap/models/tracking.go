bbb601cb6a5de6c0cd6e97b62327ca551aea7ddd
Jaydeep Mohite <jaydeep.mohite@pubmatic.com>
2024-06-13 14:19:56 +0530

    Fixed compilation error

--- modules/pubmatic/openwrap/models/tracking.go
+++ modules/pubmatic/openwrap/models/tracking.go
@@ -71,6 +71,7 @@ const (
 	ERRSUrl           = "sURL" // key represents either domain or bundle from request
 	ERRPlatform       = "pfi"
 	ERRAdvertiser     = "adv"
+	ERRTestGroup      = "tgid"
 	ERRSSAI           = "ssai"
 )
 
