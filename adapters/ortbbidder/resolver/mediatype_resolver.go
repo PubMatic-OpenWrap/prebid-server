7dbc3cce5f019e5f416c77538f7ab3346b43e96e
dhruv.sonone <dhruv.sonone@pubmatic.com>
2024-06-14 23:56:34 +0530

    moving constant

--- adapters/ortbbidder/resolver/mediatype_resolver.go
+++ adapters/ortbbidder/resolver/mediatype_resolver.go
@@ -8,6 +8,16 @@ import (
 	"github.com/prebid/prebid-server/v2/openrtb_ext"
 )
 
+// Constants used for look up in ortb response
+const (
+	mtypeKey    = "mtype"
+	bidTypeKey  = "BidType"
+	currencyKey = "Currency"
+	curKey      = "cur"
+	admKey      = "adm"
+	impIdKey    = "impid"
+)
+
 var (
 	videoRegex = regexp.MustCompile(`<VAST\s+`)
 )
