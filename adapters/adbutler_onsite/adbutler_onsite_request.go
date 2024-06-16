998f67b5634718f743a10b95b38ae1be6c2ab594
Gobi Sakthivel <gobi.sakthivel@pubmatic.com>
2024-06-16 22:47:31 +0530

    request inital parsing

--- adapters/adbutler_onsite/adbutler_onsite_request.go
+++ adapters/adbutler_onsite/adbutler_onsite_request.go
@@ -1,10 +1,34 @@
 package adbutler_onsite
 
 import (
+	"github.com/PubMatic-OpenWrap/prebid-server/errortypes"
 	"github.com/mxmCherry/openrtb/v16/openrtb2"
 	"github.com/prebid/prebid-server/adapters"
 )
 
 func (a *AdButlerOnsiteAdapter) MakeRequests(request *openrtb2.BidRequest, reqInfo *adapters.ExtraRequestInfo) ([]*adapters.RequestData, []error) {
+	siteExt, requestExt, errors := adapters.ValidateCMOnsiteRequest(request)
+	if len(errors) > 0 {
+		return nil, errors
+	}
+
+	for _, imp := range request.Imp {
+		// Parse each imp element here
+		impExt := adapters.GetImpressionExtCMOnsite(imp)
+		if impExt == nil {
+			return nil, []error{&errortypes.BadInput{
+				Message: "Missing required ext fields",
+			}}
+		}
+	
+	}
+
+	if siteExt == nil || requestExt == nil {
+		return nil, []error{&errortypes.BadInput{
+			Message: "Missing required ext fields",
+		}}
+	}
+
 	return nil,nil
 }
+
