58168e3dd21c5e82c652b31eabe7ee5bc55d08cb
dhruv.sonone <dhruv.sonone@pubmatic.com>
2024-06-17 13:32:47 +0530

    Fixing semgroup check

--- .github/workflows/helpers/pull-request-utils.js
+++ .github/workflows/helpers/pull-request-utils.js
@@ -43,6 +43,9 @@ class diffHelper {
     let diff = {}
     for (const { filename, patch } of files) {
       if (this.fileNameFilter(filename)) {
+        if (!patch) {
+          continue
+        }
         const lines = patch.split("\n")
         if (lines.length === 1) {
           continue
