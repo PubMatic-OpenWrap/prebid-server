913365269181c80597e1b8b6696ce5fa9ebf6f4f
Priyanka Bagade <priyanka.bagade@pubmatic.com>
2024-07-09 17:52:55 +0530

    removed rateLimit field of aidem bidder

--- modules/pubmatic/openwrap/adapters/default_bidder.go
+++ modules/pubmatic/openwrap/adapters/default_bidder.go
@@ -37,6 +37,9 @@ func prepareBidParamJSONDefault(params BidderParameters) (json.RawMessage, error
 			if err != nil && mapping.Required {
 				return nil, err
 			}
+			if params.AdapterName == "aidem" && mapping.KeyName == "rateLimit" {
+				delete(bidderParams, mapping.KeyName)
+			}
 		}
 	}
 
