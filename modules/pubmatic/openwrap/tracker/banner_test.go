4e7392bc1201a90cb2faecc5d0238395935edbfa
Avinash Kapre <avinash.kapre@pubmatic.com>
2024-09-12 11:02:22 +0530

    import fix 2

--- modules/pubmatic/openwrap/tracker/banner_test.go
+++ modules/pubmatic/openwrap/tracker/banner_test.go
@@ -3,10 +3,10 @@ package tracker
 import (
 	"testing"
 
-	"github.com/PubMatic-OpenWrap/prebid-server/v2/openrtb_ext"
 	"github.com/prebid/openrtb/v20/openrtb2"
 	"github.com/prebid/prebid-server/v2/modules/pubmatic/openwrap/models"
 	"github.com/prebid/prebid-server/v2/modules/pubmatic/openwrap/models/adunitconfig"
+	"github.com/prebid/prebid-server/v2/openrtb_ext"
 	"github.com/stretchr/testify/assert"
 )
 
