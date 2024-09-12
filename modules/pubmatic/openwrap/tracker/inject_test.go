77661fe9f03d7b788c7ac5c5f33809402075c10e
Avinash Kapre <avinash.kapre@pubmatic.com>
2024-09-12 11:02:22 +0530

    import fix 2

--- modules/pubmatic/openwrap/tracker/inject_test.go
+++ modules/pubmatic/openwrap/tracker/inject_test.go
@@ -3,12 +3,12 @@ package tracker
 import (
 	"testing"
 
-	"github.com/PubMatic-OpenWrap/prebid-server/v2/openrtb_ext"
 	"github.com/golang/mock/gomock"
 	"github.com/prebid/openrtb/v20/openrtb2"
 	mock_metrics "github.com/prebid/prebid-server/v2/modules/pubmatic/openwrap/metrics/mock"
 	"github.com/prebid/prebid-server/v2/modules/pubmatic/openwrap/models"
 	"github.com/prebid/prebid-server/v2/modules/pubmatic/openwrap/models/adunitconfig"
+	"github.com/prebid/prebid-server/v2/openrtb_ext"
 	"github.com/stretchr/testify/assert"
 )
 
