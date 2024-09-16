e0bc2615f00b16b3634ccaabaab8a9dffecf837e
Saurabh Narkhede <108730956+pm-saurabh-narkhede@users.noreply.github.com>
2024-09-16 11:59:11 +0530

    UOE-10822: Prometheus Stat for Bid Recovery (#851)
    * stat for bid recovery
    
    * change buckets
    
    * introduce new stat

--- modules/pubmatic/openwrap/metrics/stats/tcp_stats.go
+++ modules/pubmatic/openwrap/metrics/stats/tcp_stats.go
@@ -350,5 +350,8 @@ func (st *StatsTCP) RecordAdruleEnabled(pubId, profId string)                   
 func (st *StatsTCP) RecordAdruleValidationFailure(pubId, profId string)                       {}
 func (st *StatsTCP) RecordSignalDataStatus(pubid, profileid, signalType string)               {}
 func (st *StatsTCP) RecordPrebidCacheRequestTime(success bool, length time.Duration)          {}
+func (st *StatsTCP) RecordBidRecoveryStatus(pubID string, profile string, success bool)       {}
+func (st *StatsTCP) RecordBidRecoveryResponseTime(pubID string, profile string, responseTime time.Duration) {
+}
 func (st *StatsTCP) RecordPrebidAuctionBidResponse(publisher string, partnerName string, bidderCode string, adapterCode string) {
 }
