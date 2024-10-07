f97bb58c9809254cd59c0bd2fccf8b7b04901a13
supriya-patil <supriya.patil@pubmatic.com>
2024-10-07 14:33:32 +0530

    UOE-11098: Moved UpdateUserExtWithValidValues func to common util package

--- modules/pubmatic/openwrap/endpoints/legacy/ctv/util.go
+++ modules/pubmatic/openwrap/endpoints/legacy/ctv/util.go
@@ -1,16 +1,8 @@
 package ctv
 
 import (
-	"encoding/json"
 	"net/url"
-	"regexp"
-
-	"git.pubmatic.com/PubMatic/go-common/logger"
-	"github.com/prebid/openrtb/v20/openrtb2"
-	"github.com/prebid/prebid-server/v2/openrtb_ext"
 )
-
-var uidRegexp = regexp.MustCompile(`^(UID2|ID5|BGID|euid|PAIRID|IDL|connectid|firstid|utiq):`)
 
 func GetPubIdFromQueryParams(params url.Values) string {
 	pubId := params.Get(ORTBSitePublisherID)
@@ -18,63 +10,4 @@ func GetPubIdFromQueryParams(params url.Values) string {
 		pubId = params.Get(ORTBAppPublisherID)
 	}
 	return pubId
-}
-
-func ValidateEIDs(eids []openrtb2.EID) []openrtb2.EID {
-	validEIDs := make([]openrtb2.EID, 0, len(eids))
-	for _, eid := range eids {
-		validUIDs := make([]openrtb2.UID, 0, len(eid.UIDs))
-		for _, uid := range eid.UIDs {
-			uid.ID = uidRegexp.ReplaceAllString(uid.ID, "")
-			if uid.ID != "" {
-				validUIDs = append(validUIDs, uid)
-			}
-		}
-		if len(validUIDs) > 0 {
-			eid.UIDs = validUIDs
-			validEIDs = append(validEIDs, eid)
-		}
-	}
-	return validEIDs
-}
-
-func UpdateUserExtWithValidValues(user *openrtb2.User) {
-	if user == nil {
-		return
-	}
-
-	if user.Ext != nil {
-		var userExt openrtb_ext.ExtUser
-		err := json.Unmarshal(user.Ext, &userExt)
-		if err != nil {
-			return
-		}
-		if userExt.SessionDuration < 0 {
-			logger.Warn("Invalid sessionduration value: %v. Only positive values are allowed.", userExt.SessionDuration)
-			userExt.SessionDuration = 0
-		}
-
-		if userExt.ImpDepth < 0 {
-			logger.Warn("Invalid impdepth value: %v. Only positive values are allowed.", userExt.ImpDepth)
-			userExt.ImpDepth = 0
-		}
-		eids := ValidateEIDs(userExt.Eids)
-		userExt.Eids = nil
-		if len(eids) > 0 {
-			userExt.Eids = eids
-		}
-
-		userExtjson, err := json.Marshal(userExt)
-		if err == nil {
-			user.Ext = userExtjson
-		}
-	}
-
-	if len(user.EIDs) > 0 {
-		eids := ValidateEIDs(user.EIDs)
-		user.EIDs = nil
-		if len(eids) > 0 {
-			user.EIDs = eids
-		}
-	}
 }
