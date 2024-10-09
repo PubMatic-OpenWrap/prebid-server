47cef0e072f255b7682542e6539f25520ec7aba5
Pubmatic-Supriya-Patil <131644110+Pubmatic-Supriya-Patil@users.noreply.github.com>
2024-10-09 14:55:42 +0530

    UOE-11098: Pass user.ext.sessionduration and user.ext.impdepth ORTB request to OW partners (#922)

--- modules/pubmatic/openwrap/beforevalidationhook.go
+++ modules/pubmatic/openwrap/beforevalidationhook.go
@@ -763,7 +763,7 @@ func (m *OpenWrap) applyProfileChanges(rctx models.RequestCtx, bidRequest *openr
 	if bidRequest.User.CustomData == "" && rctx.KADUSERCookie != nil {
 		bidRequest.User.CustomData = rctx.KADUSERCookie.Value
 	}
-	ctv.UpdateUserEidsWithValidValues(bidRequest.User)
+	UpdateUserExtWithValidValues(bidRequest.User)
 
 	for i := 0; i < len(bidRequest.WLang); i++ {
 		bidRequest.WLang[i] = getValidLanguage(bidRequest.WLang[i])
