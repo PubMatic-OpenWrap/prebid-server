d68a1132cc8ec6fc1decbdea21926704e5f3d01c
pm-avinash-kapre <112699665+AvinashKapre@users.noreply.github.com>
2024-11-26 12:36:28 +0530

    UOE-11477: Cherry-Pick Displayio bidder changes (#966)

--- exchange/adapter_builders.go
+++ exchange/adapter_builders.go
@@ -1,6 +1,7 @@
 package exchange
 
 import (
+	"github.com/PubMatic-OpenWrap/prebid-server/v2/adapters/displayio"
 	"github.com/prebid/prebid-server/v2/adapters"
 	ttx "github.com/prebid/prebid-server/v2/adapters/33across"
 	"github.com/prebid/prebid-server/v2/adapters/aax"
@@ -290,6 +291,7 @@ func newAdapterBuilders() map[openrtb_ext.BidderName]adapters.Builder {
 		openrtb_ext.BidderDeepintent:          deepintent.Builder,
 		openrtb_ext.BidderDefinemedia:         definemedia.Builder,
 		openrtb_ext.BidderDianomi:             dianomi.Builder,
+		openrtb_ext.BidderDisplayio:           displayio.Builder,
 		openrtb_ext.BidderEdge226:             edge226.Builder,
 		openrtb_ext.BidderDmx:                 dmx.Builder,
 		openrtb_ext.BidderDXKulture:           dxkulture.Builder,
