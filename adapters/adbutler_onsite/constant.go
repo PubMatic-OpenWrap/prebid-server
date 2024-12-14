1ab5191bc0079cb0a10e5ae2b44fc14f934a2441
Gobi Sakthivel <gobi.sakthivel@pubmatic.com>
2024-12-14 07:51:43 +0530

    redirect fix

--- adapters/adbutler_onsite/constant.go
+++ adapters/adbutler_onsite/constant.go
@@ -4,8 +4,10 @@ const (
 	RESPONSE_SUCCESS   = "SUCCESS"
 	RESPONSE_NOADS     = "NO_ADS"
 	IMAGE_URL_TEMPLATE = "<div style='margin:0;padding:0;'><a href='ACTUAL_CLICK_URL'><img src='%s'></a></div>"
+	IMAGE_URL_TEMPLATE_TARGET = "<div style='margin:0;padding:0;'><a href='ACTUAL_CLICK_URL' target='REDIRECT_TARGET'><img src='%s'></a></div>"
 	DYNAMIC_IMAGE_URL_CLICKTEMPLATE = "<script>document.getElementById('IMPRESSION_ID').addEventListener('click', function handleAdClick(event){if(!event.target.closest('a')){window.open('ACTUAL_CLICK_URL');}}</script>"
 	DYNAMIC_IMAGE_URL_AHREFSTART = "<a href='ACTUAL_CLICK_URL'>"
+	DYNAMIC_IMAGE_URL_AHREFSTART_TARGET = "<a href='ACTUAL_CLICK_URL' target='REDIRECT_TARGET'>"
 	DYNAMIC_IMAGE_URL_AHREFEND   = "</a>"
 	Seat_AdbutlerOnsite        = "adbutler_onsite"
 	InventoryIDOnsite_Prefix   = "InventoryID_"
@@ -26,5 +28,6 @@ const (
 
 	Pattern_Click_URL = `href="(https?://[^\s]+/redirect\.spark\?[^"]+)"`
 )
+
 
 
