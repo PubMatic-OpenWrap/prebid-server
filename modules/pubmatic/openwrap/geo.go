75eaf847c6753a04077207ee9c3ec7cca566b430
saurabh-narkhede <saurabh.narkhede@pubmatic.com>
2025-01-08 12:05:05 +0530

    minor changes

--- modules/pubmatic/openwrap/geo.go
+++ modules/pubmatic/openwrap/geo.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"runtime/debug"
 	"strconv"
-	"time"
 
 	"git.pubmatic.com/PubMatic/go-common/util"
 	"github.com/golang/glog"
@@ -23,15 +22,6 @@ type geo struct {
 	SectionID   int    `json:"sectionId,omitempty"`
 }
 
-const (
-	cacheTimeout                        = time.Duration(48) * time.Hour
-	headerContentType                   = "Content-Type"
-	headerAccessControlAllowOrigin      = "Access-Control-Allow-Origin"
-	headerCacheControl                  = "Cache-Control"
-	headerContentTypeValue              = "application/json"
-	headerAccessControlAllowOriginValue = "*"
-)
-
 var gppSectionIDs = map[string]int{
 	"ca": 8,
 	"va": 9,
@@ -39,16 +29,6 @@ var gppSectionIDs = map[string]int{
 	"ut": 11,
 	"ct": 12,
 }
-
-const (
-	OriginHeaderKey     = "Origin"
-	RefererKey          = "Referer"
-	GDPRCompliance      = "GDPR"
-	USPCompliance       = "USP"
-	GPPCompliance       = "GPP"
-	StateCodeCalifornia = "ca"
-	CountryCodeUS       = "US"
-)
 
 // Handler for /geo endpoint
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -73,39 +53,35 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	_, err := strconv.Atoi(pubIdStr)
 	if err != nil {
 		glog.Errorf("[geo] error:[invalid pubid passed:%s], [requestType]:%v [url]:%v [origin]:%v [referer]:%v", err.Error(), models.EndpointGeo,
-			r.URL.RequestURI(), r.Header.Get(OriginHeaderKey), r.Header.Get(RefererKey))
+			r.URL.RequestURI(), r.Header.Get(models.HeaderOriginKey), r.Header.Get(models.HeaderRefererKey))
 		w.WriteHeader(http.StatusBadRequest)
 		metricLabels.RequestStatus = prometheus.RequestStatusBadInput
 		return
 	}
 
 	ip := util.GetIP(r)
-	w.Header().Set(headerContentType, headerContentTypeValue)
-	w.Header().Set(headerAccessControlAllowOrigin, "*")
-	success := false
+	w.Header().Set(models.HeaderContentType, models.HeaderContentTypeValue)
+	w.Header().Set(models.HeaderAccessControlAllowOrigin, "*")
+
 	geoInfo, _ := ow.geoInfoFetcher.LookUp(ip)
-	if geoInfo != nil {
-		if geoInfo.ISOCountryCode != "" {
-			success = true
-			geo := geo{
-				CountryCode: geoInfo.ISOCountryCode,
-				StateCode:   geoInfo.RegionCode,
-			}
-
-			if ow.GetFeature().IsCountryGDPREnabled(geo.CountryCode) {
-				geo.Compliance = GDPRCompliance
-			} else if geo.CountryCode == CountryCodeUS && geo.StateCode == StateCodeCalifornia {
-				geo.Compliance = USPCompliance
-			} else if sectionid, ok := gppSectionIDs[geo.StateCode]; ok {
-				geo.Compliance = GPPCompliance
-				geo.SectionID = sectionid
-			}
-
-			w.Header().Set(headerCacheControl, "max-age="+fmt.Sprint(cacheTimeout.Seconds()))
-			json.NewEncoder(w).Encode(geo)
-		}
-	}
-	if !success {
+	if geoInfo == nil || geoInfo.ISOCountryCode == "" {
 		metricEngine.RecordGeoLookupFailure(models.EndpointGeo)
+		return
 	}
+
+	geo := geo{
+		CountryCode: geoInfo.ISOCountryCode,
+		StateCode:   geoInfo.RegionCode,
+	}
+	if ow.GetFeature().IsCountryGDPREnabled(geo.CountryCode) {
+		geo.Compliance = models.GDPRCompliance
+	} else if geo.CountryCode == models.CountryCodeUS && geo.StateCode == models.StateCodeCalifornia {
+		geo.Compliance = models.USPCompliance
+	} else if sectionid, ok := gppSectionIDs[geo.StateCode]; ok {
+		geo.Compliance = models.GPPCompliance
+		geo.SectionID = sectionid
+	}
+
+	w.Header().Set(models.HeaderCacheControl, "max-age="+fmt.Sprint(models.CacheTimeout.Seconds()))
+	json.NewEncoder(w).Encode(geo)
 }
