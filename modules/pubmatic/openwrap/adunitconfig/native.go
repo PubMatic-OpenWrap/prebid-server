fae9b55852724af117e1cddcb902ac7d0a057a6e
saurabh-narkhede <saurabh.narkhede@pubmatic.com>
2025-03-25 17:19:06 +0530

    Update import version to v3

--- modules/pubmatic/openwrap/adunitconfig/native.go
+++ modules/pubmatic/openwrap/adunitconfig/native.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/prebid/openrtb/v20/openrtb2"
-	"github.com/prebid/prebid-server/v2/modules/pubmatic/openwrap/models"
-	"github.com/prebid/prebid-server/v2/modules/pubmatic/openwrap/models/adunitconfig"
+	"github.com/prebid/prebid-server/v3/modules/pubmatic/openwrap/models"
+	"github.com/prebid/prebid-server/v3/modules/pubmatic/openwrap/models/adunitconfig"
 )
 
 func UpdateNativeObjectWithAdunitConfig(rCtx models.RequestCtx, imp openrtb2.Imp, div string) (adUnitCtx models.AdUnitCtx) {
