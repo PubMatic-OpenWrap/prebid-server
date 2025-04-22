c603802b49cca4ada250bfa3602bbbc465b9a0e2
Saurabh Narkhede <108730956+pm-saurabh-narkhede@users.noreply.github.com>
2025-04-22 15:19:46 +0530

    Update video.go

--- modules/pubmatic/openwrap/tracker/video.go
+++ modules/pubmatic/openwrap/tracker/video.go
@@ -15,7 +15,7 @@ func injectVideoCreativeTrackers(rctx models.RequestCtx, bid openrtb2.Bid, video
 	}
 
 	skipTracker := false
-	if rctx.Endpoint == models.EndpointAppLovinMax  || rctx.Endpoint == models.EndpointGoogleSDK {
+	if rctx.Endpoint == models.EndpointAppLovinMax || rctx.Endpoint == models.EndpointGoogleSDK {
 		skipTracker = true
 	}
 
