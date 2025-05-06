558bc4b6e4f2ae80de50a909215f569417197216
saurabh-narkhede <saurabh.narkhede@pubmatic.com>
2025-05-06 17:32:11 +0530

    change

--- modules/pubmatic/openwrap/models/openwrap.go
+++ modules/pubmatic/openwrap/models/openwrap.go
@@ -267,10 +267,11 @@ type HashSet map[string]struct{}
 type ProfileAdUnitMultiFloors map[int]map[string]MultiFloors
 
 type MultiFloors struct {
-	IsEnabled bool    `json:"isEnabled,omitempty"`
-	Tier1     float64 `json:"tier1,omitempty"`
-	Tier2     float64 `json:"tier2,omitempty"`
-	Tier3     float64 `json:"tier3,omitempty"`
+	IsActive  bool   `json:"isActive,omitempty"`
+	Tier1     string `json:"tier1,omitempty"`
+	Tier2     string `json:"tier2,omitempty"`
+	Tier3     string `json:"tier3,omitempty"`
+	ProfileID int    `json:"profileId,omitempty"`
 }
 
 func (w WinningBids) IsWinningBid(impId, bidId string) bool {
