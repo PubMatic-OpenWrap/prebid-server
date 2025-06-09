55d39d62b546422b2636b2afd734bbfc14cc63db
supriya-patil <supriya.patil@pubmatic.com>
2025-06-09 11:34:55 +0530

    refactor: extract VAST XML enrichment logic into separate UpdateADMWithAdvCat function

--- modules/pubmatic/openwrap/feature/googleSSU.go
+++ modules/pubmatic/openwrap/feature/googleSSU.go
@@ -1,5 +1,3 @@
-// function to enrich vast xml with google ssu cat and advertiser and call from auctionresp hook and HB
-
 package feature
 
 import (
@@ -10,28 +8,32 @@ import (
 func EnrichVASTWithSSUFeature(bidResponse *openrtb2.BidResponse, vastXMLHandler parser.VASTXMLHandler) {
 	for _, seatBid := range bidResponse.SeatBid {
 		for i := range seatBid.Bid {
-			bid := &seatBid.Bid[i] // use pointer so changes reflect
+			bid := &seatBid.Bid[i]
 			if bid.AdM == "" {
 				continue
 			}
-
-			if err := vastXMLHandler.Parse(bid.AdM); err != nil {
-				continue
-			}
-			if len(bid.Cat) > 0 {
-				adm, err := vastXMLHandler.AddCategoryTag(bid.Cat)
-				if err == nil {
-					bid.AdM = adm
-				}
-			}
-
-			if len(bid.ADomain) > 0 {
-				adm, err := vastXMLHandler.AddAdvertiserTag(bid.ADomain[0])
-				if err == nil {
-					bid.AdM = adm
-				}
-			}
+			bid.AdM = UpdateADMWithAdvCat(vastXMLHandler, bid.AdM, bid.Cat, bid.ADomain)
 		}
 	}
 
+}
+
+func UpdateADMWithAdvCat(vastXMLHandler parser.VASTXMLHandler, AdM string, cat []string, adomain []string) string {
+	if err := vastXMLHandler.Parse(AdM); err != nil {
+		return AdM
+	}
+	if len(cat) > 0 {
+		adm, err := vastXMLHandler.AddCategoryTag(cat)
+		if err == nil {
+			AdM = adm
+		}
+	}
+
+	if len(adomain) > 0 {
+		adm, err := vastXMLHandler.AddAdvertiserTag(adomain[0])
+		if err == nil {
+			AdM = adm
+		}
+	}
+	return AdM
 }
